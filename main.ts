6855e04656beb475d9146ac2ed8585e187737483
JamieGrantham <113033674+JamieGrantham@users.noreply.github.com>
2022-10-05 09:08:30 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+game.splash("Welcome to the pizza cost calculator")
+let D = game.askForNumber("Enter Diameter of pizza (in)")
+let Cost = 1.75 + 0.5 * D
+let Total_cost = D * 1.13
+let round = Math.round(Total_cost * 100)
+let final = round / 100
+game.splash(final)
