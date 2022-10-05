05894fd0dcc157b0e14d8b933a03acf7b4631834
JamieGrantham <113033674+JamieGrantham@users.noreply.github.com>
2022-10-05 09:13:55 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 game.splash("Welcome to the pizza cost calculator")
 let D = game.askForNumber("Enter Diameter of pizza (in)")
 let Cost = 1.75 + 0.5 * D
-let Total_cost = D * 1.13
+let Total_cost = Cost * 1.13
 let round = Math.round(Total_cost * 100)
 let final = round / 100
 game.splash(final)
+pause(100)
+game.reset()
